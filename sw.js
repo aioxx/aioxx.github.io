27f71785aa0544729e81eceb1372d534b419b9cb
Aioxx <120001849+aioxx@users.noreply.github.com>
2024-06-03 19:06:52 +0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,30 @@
+// sw.js
+
+// set names for both precache & runtime cache
+workbox.core.setCacheNameDetails({
+    prefix: 'aioxx.github.io',
+    suffix: 'v1',
+    precache: 'precache',
+    runtime: 'runtime-cache'
+});
+
+// let Service Worker take control of pages ASAP
+workbox.skipWaiting();
+workbox.clientsClaim();
+
+// default to `networkFirst` strategy
+workbox.routing.setDefaultHandler(workbox.strategies.networkFirst());
+
+// let Workbox handle our precache list
+// NOTE: This will be populated by jekyll-workbox-plugin.
+workbox.precaching.precacheAndRoute([]);
+
+// use `Stale-while-revalidate` strategy for images and fonts.
+workbox.routing.registerRoute(
+    /images/,
+    workbox.strategies.staleWhileRevalidate()
+);
+workbox.routing.registerRoute(
+    /^https?:\/\/fonts\.googleapis\.com/,
+    workbox.strategies.staleWhileRevalidate()
+);
